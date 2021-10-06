46ac0ee7e26de92e874f51159deda1acf7c03a8f
Nicholas Farkas <nicholaswfarkas@gmail.com>
2021-10-06 03:30:36 -0400

    Created Skill View Component and Test Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Profile } from 'src/app/models/profile.model';
 import { Skill } from 'src/app/models/skill.model';
+import { WorkExperience } from './models/work-experience.model';
 
 export interface Experience {
   language: string;
@@ -17,13 +18,66 @@ const ELEMENT_DATA: Experience[] = [
   {language: "R", yearsOfExperience: 1}
 ];
 
+let pro = new Profile
+(
+  "Nicholas Farkas",
+  "nicholaswfarkas.dev@gmail.com",
+  "313-969-3310",
+  [
+    new WorkExperience
+    (
+      "Software Engineer Intern",
+      "Rocket Mortgage",
+      new Date("May, 2019"),
+      new Date("August, 2019"),
+      "Detroit",
+      [
+        "Engineering",
+        "Software Engineering",
+        "C#",
+        ".Net Core"
+      ]
+    ),
+    new WorkExperience
+    (
+      "Software Engineer Intern",
+      "Rocket Mortgage",
+      new Date("June, 2020"),
+      new Date("August, 2020"),
+      "Detroit",
+      [
+        "Engineering",
+        "Software Engineering",
+        "Salesforce",
+        "SQL"
+      ]
+    )
+  ],
+  [
+    new Skill
+    (
+      "C#",
+      "Use C# for .Net Core Backend Developement",
+      2,
+      2,
+      [
+        ".Net Core"
+      ]
+    )
+  ],
+  "nickwfarkas.github.io",
+  "nickfarkas9"
+);
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'personal-portfolio';
   displayedColumns: string[] = ['language', 'years-of-experience'];
   dataSource = ELEMENT_DATA;
+  profile = pro;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Profile } from 'src/app/models/profile.model';
 import { Skill } from 'src/app/models/skill.model';
+import { SkillsViewComponent } from './components/skills-view/skills-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsViewComponent
   ],
   imports: [
     BrowserModule,
