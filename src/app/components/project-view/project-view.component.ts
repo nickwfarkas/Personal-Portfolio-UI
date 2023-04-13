b4726964f5ff7bf1bfaab57def66871c6345bbfe
Nicholas Farkas <nicholaswfarkas@gmail.com>
2023-04-13 18:48:34 -0400

    Final for Project

--- src/app/components/project-view/project-view.component.ts
+++ src/app/components/project-view/project-view.component.ts
@@ -1,7 +1,6 @@
+import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import fetch from 'node-fetch';
-import { from } from 'rxjs';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'project-view',
@@ -10,14 +9,18 @@ import { from } from 'rxjs';
 })
 export class ProjectViewComponent implements OnInit {
   repository: any;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
     this.showRepos()
   }
 
+  goToUrl(url: string): void {
+    this.document.location.href = url;
+  }
+
   getRepos() {
-    let token = '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';
+    let token = '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';
     let url = 'https://api.github.com/user/repos';
     let options = {  
       method: 'GET',
@@ -28,16 +31,13 @@ export class ProjectViewComponent implements OnInit {
     return this.http.get(url, options);
   }
 
+  convertDate(date: string) {
+    let newDate = new Date(date);
+    return newDate.toLocaleDateString();
+  }
+
   showRepos() {
     this.getRepos()
     .subscribe(repo => this.repository = repo);
-  //     .subscribe((data: any) => this.testData = {
-  //       id: data.id
-  //     });
-  // }
-}
-
-// export interface Repository {
-//   userName: string;
-//   avatarUrl: string;
+  }
 }
